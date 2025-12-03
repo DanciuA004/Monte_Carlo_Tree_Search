9922af3fdf932312affff8422e02ea8ea5f53276
DanciuA004 <danciua004@gmail.com>
2025-12-03 20:18:17 +0000

    fix: coppied over all connect4 poc

---
+++ productTMP/src/main/java/poc_connect4/Connect4Player.java
@@ -0,0 +1,30 @@
+package poc_connect4;
+
+/**
+ * This is a class that hold the information for each player, such as their name
+ * and disc colour.
+ */
+public class Connect4Player {
+  String name;
+  char disc;
+
+  /**
+   * A constructor for the player class.
+   *
+   * @param name sets the name of the player.
+   * @param disc set the disc colour for the player.
+   */
+  public Connect4Player(String name, char disc) {
+    this.name = name;
+    this.disc = disc;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public char getDisc() {
+    return disc;
+  }
+}
+
